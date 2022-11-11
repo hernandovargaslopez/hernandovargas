52c21da644192c3c50008dc9b8e20b5685e9bdb4
HernandoVargasL <91139093+HernandoVargasL@users.noreply.github.com>
2022-11-10 21:30:57 -0500

    first push

---
+++ index.js
@@ -0,0 +1,44 @@
+let homeSwiper = new Swiper(".swiper", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+
+//nav smooth
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        if (target.length) {
+          $("html, body").animate(
+            {
+              scrollTop: target.offset().top - 120,
+            },
+            500
+          );
+          return false;
+        }
+      }
+    });
+});
